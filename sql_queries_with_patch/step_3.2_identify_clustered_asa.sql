a181ceab7dc7f556755cd7fbba1e1417d6bfc1b5
Omar ElMaria <omar.elmaria@deliveryhero.com>
2022-09-29 12:57:40 +0200

    add queries with patch + sqlfluff file

---
+++ sql_queries_with_patch/step_3.2_identify_clustered_asa.sql
@@ -0,0 +1,28 @@
+-- Step 3.2: Identify the ASAs that have been clustered
+CREATE OR REPLACE TABLE `dh-logistics-product-ops.pricing.parent_child_asa_linking_loved_brands_scaled_code` AS
+WITH original_asa AS (
+    SELECT DISTINCT entity_id, country_code, asa_name, asa_id
+    FROM `dh-logistics-product-ops.pricing.asa_setups_loved_brands_scaled_code`
+),
+
+parent_and_child_asa AS (
+  SELECT 
+    a.*,
+    b.entity_id AS entity_id_linked_asa,
+    b.country_code AS country_code_linked_asa,
+    b.asa_name AS linked_asa,
+  FROM original_asa a
+  CROSS JOIN original_asa b
+)
+
+SELECT 
+    a.*,
+    -- If child = parent + "_LB" OR parent + '_LB' = child
+    CASE WHEN LOWER(a.asa_name) = LOWER(CONCAT(linked_asa, '_LB')) OR LOWER(CONCAT(a.asa_name, '_LB')) = LOWER(linked_asa) THEN TRUE ELSE FALSE END AS is_asa_clustered,
+    TRIM(a.asa_name, '_LB') AS asa_common_name
+FROM parent_and_child_asa a
+WHERE TRUE 
+    AND CASE WHEN LOWER(a.asa_name) = LOWER(CONCAT(linked_asa, '_LB')) OR LOWER(CONCAT(a.asa_name, '_LB')) = LOWER(linked_asa) THEN TRUE ELSE FALSE END = TRUE
+    AND a.entity_id = a.entity_id_linked_asa
+    AND a.country_code = a.country_code_linked_asa
+ORDER BY 1,2,3;
---
+++ sql_queries_with_patch/step_3.3_add_master_asa_id.sql
@@ -0,0 +1,27 @@
+-- Step 3.3: Add the master_asa_id to the table that contains the ASA setups in each country
+CREATE OR REPLACE TABLE `dh-logistics-product-ops.pricing.asa_setups_loved_brands_scaled_code` AS
+SELECT 
+    a.region,
+    a.entity_id,
+    a.country_code,
+    a.asa_id,
+    CASE WHEN b.is_asa_clustered = TRUE THEN MAX(asa_id) OVER(PARTITION BY a.entity_id, a.country_code) + 1 ELSE a.asa_id END AS master_asa_id,
+    a.asa_name,
+    COALESCE(b.asa_common_name, a.asa_name) AS asa_common_name,
+    COALESCE(b.is_asa_clustered, FALSE) AS is_asa_clustered,
+    a.asa_priority, -- The priority of the automatic assignment. This does not always match the priority ID in the DPS UI
+    a.asa_setup_active_from,
+    a.asa_setup_active_from_max_date,
+    a.asa_setup_active_to,
+    a.vendor_code,
+    a.scheme_id,
+    a.vendor_group_price_config_id,
+    a.condition_priority,
+    a.time_condition_id,
+    a.customer_condition_id,
+    a.pc_setup_active_from,
+    a.pc_setup_active_from_max_date,
+    a.pc_setup_active_to,
+    a.vendor_count_caught_by_asa
+FROM `dh-logistics-product-ops.pricing.asa_setups_loved_brands_scaled_code` a
+LEFT JOIN `dh-logistics-product-ops.pricing.parent_child_asa_linking_loved_brands_scaled_code` b USING (entity_id, country_code, asa_id);
